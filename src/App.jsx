f527f36bc2056e91802caca2fe2e4d4a5d1c05d6
rajul2911 <rajulgupta2911@gmail.com>
2026-09-04 21:23:53 +0530

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import PageAnimation from "./pages/PageAnimation";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="page-animation" element={<PageAnimation />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="page-animation" element={<PageAnimation />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 };
 
