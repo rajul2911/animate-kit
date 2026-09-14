7e60611ca2e025b712dfd65ad84a5d4195a4b226
rajul2911 <rajulgupta2911@gmail.com>
2026-09-14 22:13:29 +0530

    Added menu animatin codereusable

--- src/RouteConfig/MenuAnimationRoutes.jsx
+++ src/RouteConfig/MenuAnimationRoutes.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MenuAnimation from '../pages/MenuAnimation'
 import MenuOne from '../components/MenuAnimation/MenuOne/MenuOne'
+import { menucode } from '../files&codes/MenuAnimationCodeShow'
+import CodeUiReusable from '../utils/CodeUiReusable'
 
 const MenuAnimationRoutes = () => {
   return (
    <>
-   <Routes>
-    <Route index element={<MenuAnimation/>}/>
-    <Route path="menu-one" element={<MenuOne/>}/>
-
-
-   </Routes>
+    <CodeUiReusable
+      animations={menucode}
+      eyebrow="Menu Animation"
+      title="Menu Animation"
+      description="Explore different menu animations and their source code."
+       backRoute="/"
+    />
    
    </>
   )
